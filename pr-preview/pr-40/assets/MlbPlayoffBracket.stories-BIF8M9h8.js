f8a6a3fb1aefe165871391d091b99a9c85c0097c
onlyzoran <onlyzoran@users.noreply.github.com>
2026-08-21 12:55:12 +0000

    Deploy preview for PR 40 🛫

---
+++ pr-preview/pr-40/assets/MlbPlayoffBracket.stories-BIF8M9h8.js
@@ -0,0 +1,15 @@
+import{w as r}from"./storyHelpers-CiLmDOdr.js";import{_ as a}from"./MlbPlayoffBracketPattern-frczbwSz.js";import"./iframe-BGSuGtXa.js";import"./preload-helper-DCvky1p-.js";import"./bracket-GPI93fue.js";import"./cn-DaT-yT0k.js";import"./utils-DlQ6874p.js";import"./MlbLeagueBracketPattern-DmoeNjIq.js";import"./index-LDCaZGMm.js";import"./Primitive-1ehU8uU-.js";import"./index-DCK1vsYF.js";const u={title:"Win Predict AI / Patterns / MlbPlayoffBracket",component:a,tags:["autodocs"],parameters:{layout:"padded"}},t={render:()=>({components:{MlbPlayoffBracketPattern:a},template:"<MlbPlayoffBracketPattern />"})},e={render:()=>({components:{MlbPlayoffBracketPattern:a},template:'<MlbPlayoffBracketPattern layout="mobile" />'})},{ZincLight:k,ZincDark:b,SlateTealLight:y,SlateTealDark:M,ClaudePlusLight:g,ClaudePlusDark:B}=r(t);t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  render: () => ({
+    components: {
+      MlbPlayoffBracketPattern
+    },
+    template: '<MlbPlayoffBracketPattern />'
+  })
+}`,...t.parameters?.docs?.source}}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  render: () => ({
+    components: {
+      MlbPlayoffBracketPattern
+    },
+    template: '<MlbPlayoffBracketPattern layout="mobile" />'
+  })
+}`,...e.parameters?.docs?.source}}};const D=["Default","Mobile","ZincLight","ZincDark","SlateTealLight","SlateTealDark","ClaudePlusLight","ClaudePlusDark"];export{B as ClaudePlusDark,g as ClaudePlusLight,t as Default,e as Mobile,M as SlateTealDark,y as SlateTealLight,b as ZincDark,k as ZincLight,D as __namedExportsOrder,u as default};
