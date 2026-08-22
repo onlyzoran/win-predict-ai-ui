cae98c02f58a9293a3090f457bd07c2d4926cc19
onlyzoran <42581800+onlyzoran@users.noreply.github.com>
2026-08-22 06:44:15 +0000

    Deploying to gh-pages from @ onlyzoran/win-predict-ai-ui@d069f72b858c1689b31a66efb78a9527b33b661a 🚀

---
+++ assets/StandingsRankChart.stories-C854Wsi1.js
@@ -0,0 +1,11 @@
+import{w as a}from"./storyHelpers-CiLmDOdr.js";import{m as r,_ as e}from"./rankSeries-DrqLZuSh.js";import"./iframe-DqO-fblW.js";import"./preload-helper-CWSTMizN.js";import"./index-CJHlksTu.js";import"./useId-DNnsI3Aa.js";import"./createContext-BiuJ0FWR.js";import"./cn-DaT-yT0k.js";import"./Primitive-wDvBEZQ6.js";import"./index-OvRJ06sW.js";import"./_commonjsHelpers-CqkleIqs.js";import"./ChartTooltipContent-BAwpC7AK.js";const h={title:"Win Predict AI / Patterns / StandingsRankChart",component:e,tags:["autodocs"],parameters:{layout:"padded"}},t={render:()=>({components:{StandingsRankChartPattern:e},setup:()=>({series:r}),template:'<StandingsRankChartPattern :series="series" />'})},{ZincLight:S,ZincDark:P,SlateTealLight:C,SlateTealDark:D,ClaudePlusLight:R,ClaudePlusDark:f}=a(t);t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  render: () => ({
+    components: {
+      StandingsRankChartPattern
+    },
+    setup: () => ({
+      series: mockRankSeries
+    }),
+    template: '<StandingsRankChartPattern :series="series" />'
+  })
+}`,...t.parameters?.docs?.source}}};const L=["Default","ZincLight","ZincDark","SlateTealLight","SlateTealDark","ClaudePlusLight","ClaudePlusDark"];export{f as ClaudePlusDark,R as ClaudePlusLight,t as Default,D as SlateTealDark,C as SlateTealLight,P as ZincDark,S as ZincLight,L as __namedExportsOrder,h as default};
