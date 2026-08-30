bebf94f590d57ca2bd787c28937eb4652404b983
onlyzoran <42581800+onlyzoran@users.noreply.github.com>
2026-08-30 13:25:42 +0000

    Deploying to gh-pages from @ onlyzoran/win-predict-ai-ui@df8c5753f50f5757f09e6f9d309d0a2477794831 🚀

---
+++ assets/TournamentStandingsPanel.stories-7h3GmJKb.js
@@ -0,0 +1,41 @@
+import{w as m}from"./storyHelpers-C8GjIxNC.js";import{_ as t}from"./TournamentStandingsPanelPattern-BbAjh8as.js";import{m as s,p as o}from"./teams-C7RLY5nc.js";import"./iframe-CzdnysWf.js";import"./preload-helper-CWSTMizN.js";import"./index-LQk222Qh.js";import"./useId-5wv-v-Eg.js";import"./createContext-BXJSKKLX.js";import"./cn-DaT-yT0k.js";import"./Primitive-CUUinPar.js";import"./index-DQKN44HC.js";import"./_commonjsHelpers-CqkleIqs.js";import"./utils-DlQ6874p.js";import"./WinProbabilityPieTooltipPattern-B6Kg5tGW.js";import"./index-B3eTbHjD.js";import"./Separator-Dsc305NL.js";import"./index-BzX9UG9n.js";const k={title:"Win Predict AI / Patterns / TournamentStandingsPanel",component:t,tags:["autodocs"],parameters:{layout:"padded"}},a={render:()=>({components:{TournamentStandingsPanelPattern:t},setup:()=>({teams:s}),template:'<TournamentStandingsPanelPattern :teams="teams" />'})},e={render:()=>({components:{TournamentStandingsPanelPattern:t},setup:()=>({teams:s}),template:'<TournamentStandingsPanelPattern :teams="teams" show-chart />'})},n={render:()=>({components:{TournamentStandingsPanelPattern:t},setup:()=>({teams:s}),template:'<TournamentStandingsPanelPattern :teams="teams" compact />'})},r={render:()=>({components:{TournamentStandingsPanelPattern:t},setup:()=>({teams:o}),template:'<TournamentStandingsPanelPattern :teams="teams" />'})},{ZincLight:L,ZincDark:O,SlateTealLight:_,SlateTealDark:w,ClaudePlusLight:Z,ClaudePlusDark:x}=m(e);a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  render: () => ({
+    components: {
+      TournamentStandingsPanelPattern
+    },
+    setup: () => ({
+      teams: mlbWorldSeriesTeams
+    }),
+    template: '<TournamentStandingsPanelPattern :teams="teams" />'
+  })
+}`,...a.parameters?.docs?.source}}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  render: () => ({
+    components: {
+      TournamentStandingsPanelPattern
+    },
+    setup: () => ({
+      teams: mlbWorldSeriesTeams
+    }),
+    template: '<TournamentStandingsPanelPattern :teams="teams" show-chart />'
+  })
+}`,...e.parameters?.docs?.source}}};n.parameters={...n.parameters,docs:{...n.parameters?.docs,source:{originalSource:`{
+  render: () => ({
+    components: {
+      TournamentStandingsPanelPattern
+    },
+    setup: () => ({
+      teams: mlbWorldSeriesTeams
+    }),
+    template: '<TournamentStandingsPanelPattern :teams="teams" compact />'
+  })
+}`,...n.parameters?.docs?.source}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  render: () => ({
+    components: {
+      TournamentStandingsPanelPattern
+    },
+    setup: () => ({
+      teams: probabilityOnlyTeams
+    }),
+    template: '<TournamentStandingsPanelPattern :teams="teams" />'
+  })
+}`,...r.parameters?.docs?.source}}};const A=["Default","WithChart","Compact","ProbabilityOnly","ZincLight","ZincDark","SlateTealLight","SlateTealDark","ClaudePlusLight","ClaudePlusDark"];export{x as ClaudePlusDark,Z as ClaudePlusLight,n as Compact,a as Default,r as ProbabilityOnly,w as SlateTealDark,_ as SlateTealLight,e as WithChart,O as ZincDark,L as ZincLight,A as __namedExportsOrder,k as default};
